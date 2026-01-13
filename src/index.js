83bb3e1c666c74d868950791bc7a93a1e185e129
Yashoda <01fe23bcs141@kletech.ac.in>
2026-01-13 08:17:23 +0530

    chore: prepare for Render deployment

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ async function main() {
   await createAdminUser();
   app.listen(app.get("port"));
 
-  console.log("Server on port", app.get("port"));
+  console.log("🚀 Notes App running on port:", app.get("port"));
   console.log("Environment:", process.env.NODE_ENV);
 }
 
